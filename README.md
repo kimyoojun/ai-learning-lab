1b5c65ef50e88eb2939b652a253992f53a592904
kimyoojun <anano3134@gmail.com>
2026-05-24 15:12:56 +0900

    Python OpenCv 이미지 출력 공부

--- README.md
+++ README.md
@@ -9,3 +9,32 @@ python 환경변수 설정
  26.05.23 NumPy 시작
 ----------
 pip install numpy
+
+### 이미지 출력
+
+- flags
+    - cv2.IMREAD_UNCHANGED : 원본 사용
+    - cv2.IMREAD_GRAYSCALE : 1 채널, 그레이스케일 적용
+    - cv2.IMREAD_COLOR : 3 채널, BGR 이미지 사용
+    - cv2.IMREAD_ANYDEPTH : 이미지에 따라 정밀도를 16/32비트 또는 8비트로 사용
+    - cv2.IMREAD_ANYCOLOR : 가능한 3 채널, 색상 이미지로 사용
+    - cv2.IMREAD_REDUCED_GRAYSCALE_2 : 1 채널, 1/2 크기, 그레이스케일 적용
+    - cv2.IMREAD_REDUCED_GRAYSCALE_4 : 1 채널, 1/4 크기, 그레이스케일 적용
+    - cv2.IMREAD_REDUCED_GRAYSCALE_8 : 1 채널, 1/8 크기, 그레이스케일 적용
+    - cv2.IMREAD_REDUCED_COLOR_2 : 3 채널, 1/2 크기, BGR 이미지 사용
+    - cv2.IMREAD_REDUCED_COLOR_4 : 3 채널, 1/4 크기, BGR 이미지 사용
+    - cv2.IMREAD_REDUCED_COLOR_8 : 3 채널, 1/8 크기, BGR 이미지 사용
+
+```
+height, width, channel = image(변수).shape
+print(height, width, channel)
+```
+위 코드처럼 이미지의 높이(height), 너비(width), 채널(channel)의 값을 확인할 수 있음
+
+이미지의 속성은 크기, 정밀도, 채널을 주요한 속성으로 사용함
+- 크기: 이미지의 **높이**와 **너비**를 의미함
+- 정밀도: 이미지의 처리 결과의 **정밀성**을 의미함
+- 채널: 이미지의 **색상 정보**를 의미함
+- TIP: **유효 비트가 많을 수록 더 정밀함**
+- TIP: 채널이 3일 경우는 **다색 이미지**, 채널이 1일 경우 **단색 이미지**
+
--- opencv_python_.py
+++ opencv_python_.py
@@ -1,26 +1,41 @@
 import cv2
 
-# 카메라 출력
+# # 카메라 출력
 
-# 카메라에서 정보를 받아오는 코드
-capture = cv2.VideoCapture(0)   # cv2.VideoCapture(index) index는 카메라 번호 내장 카메라는 0 외장카메라는 1~n
-# 카메라 속성 설정
-# capture.set(propid, value)로 카메라의 속성(propid) 값(value)을 설정
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # 카메라 너비는 640
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 카메라 높이는 480
+# # 카메라에서 정보를 받아오는 코드
+# capture = cv2.VideoCapture(0)   # cv2.VideoCapture(index) index는 카메라 번호 내장 카메라는 0 외장카메라는 1~n
+# # 카메라 속성 설정
+# # capture.set(propid, value)로 카메라의 속성(propid) 값(value)을 설정
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # 카메라 너비는 640
+# capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 카메라 높이는 480
 
-# 반복문을 활용하여 카메라에서 프레임을 지속적으로 받아옴
-while cv2.waitKey(33) < 0:  # cv2.waitKey는 지정된 시간 동안 키 입력이 있을때까지 프로그램 지연
-                            # cv2.waitKey(delay) delay는 지연시간
-                            # while cv2.waitKey(33) != ord('q'): 이런 식의 반복문이면 q키가 입력되면 반복문 종료
-    ret, frame = capture.read() # 프레임 읽기 메서드 capture.read()를 이용하여 카메라의 상태 및 프레임을 받아옴
-                                # ret은 카메라의 상태가 저장되면 카메라가 동작하면 True를 반환, 작동하지 않으면 False를 반환
-                                # frame에는 현재시점의 프레임이 저장
-    cv2.imshow("VideoFrame", frame) # 이미지 표시 함수 cv2.show 사용하여 특정 윈도우 창에 이미지를 띄움
-                                    # cv2.imshow(winname, mat)으로 윈도우 창의 제목(winname) 이미지(mat)를 할당
-                                    # winname은 문자열로 표시, 할당한 문자열이 변수와 비슷한 역할을함
-                                    # 이 코드는 VideoFrame이라는 윈도우 창에 프레임이 표시됨
+# # 반복문을 활용하여 카메라에서 프레임을 지속적으로 받아옴
+# while cv2.waitKey(33) < 0:  # cv2.waitKey는 지정된 시간 동안 키 입력이 있을때까지 프로그램 지연
+#                             # cv2.waitKey(delay) delay는 지연시간
+#                             # while cv2.waitKey(33) != ord('q'): 이런 식의 반복문이면 q키가 입력되면 반복문 종료
+#     ret, frame = capture.read() # 프레임 읽기 메서드 capture.read()를 이용하여 카메라의 상태 및 프레임을 받아옴
+#                                 # ret은 카메라의 상태가 저장되면 카메라가 동작하면 True를 반환, 작동하지 않으면 False를 반환
+#                                 # frame에는 현재시점의 프레임이 저장
+#     cv2.imshow("VideoFrame", frame) # 이미지 표시 함수 cv2.show 사용하여 특정 윈도우 창에 이미지를 띄움
+#                                     # cv2.imshow(winname, mat)으로 윈도우 창의 제목(winname) 이미지(mat)를 할당
+#                                     # winname은 문자열로 표시, 할당한 문자열이 변수와 비슷한 역할을함
+#                                     # 이 코드는 VideoFrame이라는 윈도우 창에 프레임이 표시됨
 
-capture.release()   # 메모리 해제 메서드 capture.release()로 카메라 장치에서 받아온 메모리 해제
-cv2.destroyAllWindows() # 윈도우 창 제거 함수 cv2.destroyAllWindows()를 이용하여 모든 윈도우 창을 닫음
-                        # 특정 윈도우 창만 닫으려면 cv2.destroyAllWindows(winname) 형식으로 특정 윈도우 창만 닫음
+# capture.release()   # 메모리 해제 메서드 capture.release()로 카메라 장치에서 받아온 메모리 해제
+# cv2.destroyAllWindows() # 윈도우 창 제거 함수 cv2.destroyAllWindows()를 이용하여 모든 윈도우 창을 닫음
+#                         # 특정 윈도우 창만 닫으려면 cv2.destroyAllWindows(winname) 형식으로 특정 윈도우 창만 닫음
+
+
+
+# 이미지 출력
+
+# 이미지 입력 함수 cv2.imread()를 통해 로컬 경로의 이미지를 읽어옴
+image = cv2.imread("C:\study\image\cat1.jpg", cv2.IMREAD_ANYCOLOR)  # cv2.imread(fileName, flags)
+                                                                    # 파일경로(fileName)의 이미지 파일을 플래그(flags) 설정에 따라 불러옴
+                                                                    # 파일경로(fileName)은 상대경로, 절대경로 두가지 방법으로 불러올수 있음
+                                                                    # 플래그(flags)는 이미지를 초기에 불러올때 초기 상태를 의미함
+
+# 이미지 표시 함수 cv2.imshow()와 키 입력 대기 함수 cv2.waitKey()로 윈도우창에 이미지를 띄움                                                                 
+cv2.imshow("Cat", image)
+cv2.waitKey()   # 키 대기 함수를 입력하지 않을 경우 윈도우 창이 유지되지 않고 프로그램 종료
+cv2.destroyAllWindows() # 키 입력후 윈도우 창 제거 함수 cv2.destroyAllWindows()를 이용하여 모든 윈도우 창을 닫음
